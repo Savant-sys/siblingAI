068255fb39dccc8a6cabd842d62ad294d23f6fbd
Savant-sys <michaelkhuri@gmail.com>
2023-12-03 22:06:17 -0800

    Update next.config.js

--- siblingai/next.config.js
+++ siblingai/next.config.js
@@ -1,15 +1,6 @@
-// siblingai/next.config.js
-module.exports = {
-    rewrites() {
-      return {
-        beforeFiles: [
-          // Custom rewrite rules specific to the "siblingai" directory
-          {
-            source: '/custom-path/:path*', // Match specific paths
-            destination: '/custom-handler/:path*', // Redirect to a different path
-          },
-        ],
-      };
-    },
-  };
-  
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+};
+
+module.exports = nextConfig;
