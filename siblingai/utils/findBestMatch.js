0603621f6c731190c2f1cb8afebc35ef56f6d545
Savant-sys <michaelkhuri@gmail.com>
2023-12-04 12:56:08 -0800

    clean up

--- siblingai/utils/findBestMatch.js
+++
@@ -1,9 +0,0 @@
-// utils/findBestMatch.js
-import stringSimilarity from 'string-similarity';
-
-function findBestMatch(input, possibilities) {
-  const matches = stringSimilarity.findBestMatch(input, possibilities);
-  return matches.bestMatch.target;
-}
-
-export default findBestMatch;
