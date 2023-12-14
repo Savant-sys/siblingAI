2dc03186b9082a6f91dc8c4ad0ec30b13b120426
Michael Khuri <michaelkhuri@gmail.com>
2023-12-13 19:04:34 -0800

    Update learn.js

--- siblingai/pages/api/chat/learn.js
+++ siblingai/pages/api/chat/learn.js
@@ -1,17 +1,20 @@
 // pages/api/chat/learn.js
 
 // learn.js is for importing data into brain.json when the question doesnt exist from the user's response
+// otherwise the existing response will be returning to chat.js from brain.js
 import fs from 'fs';
 import path from 'path';
 
 const brainFilePath = path.join(process.cwd(), 'data', 'brain.json');
 
+// returns the response from brain.js to chat.js in order to reply back
 const loadBrain = () =>
 {
   const data = fs.readFileSync(brainFilePath, 'utf8');
   return JSON.parse(data).questions;
 };
 
+// saves the new response into brain.js after learning new things from the user
 const saveBrain = (brain) =>
 {
   fs.writeFileSync(brainFilePath, JSON.stringify({ questions: brain }, null, 2), 'utf8');
